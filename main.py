aa6b0489de95a34bc1e57aac5a521228b3dac1d4
Carlos Ayala <cayalav17@gmail.com>
2021-09-13 16:22:12 +0000

    Change(num1)

---
+++ main.py
@@ -0,0 +1,11 @@
+#Ejercicios Taller Condicional - Carlos Ayala
+
+# Ejercicio 1
+def num1(n,total):
+  if n > 3:
+    total=total*0.7
+    print("Total a pagar: "+str(total))
+  else:
+    total=total*0.9
+    print("Total a pagar: "+str(total)) 
+  print("######################################################") 
