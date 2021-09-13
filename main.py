266746bbd188fcbc6fb61fe48f58bc9e93e532e2
Carlos Ayala <cayalav17@gmail.com>
2021-09-13 16:30:39 +0000

    Change(num3)

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def num1(n,total):
     print("Total a pagar: "+str(total)) 
   print("######################################################") 
 
-  # Ejercicio 2
+# Ejercicio 2
 def num2(random, total):
   if random < 74:
     total=total*0.15
@@ -18,4 +18,14 @@ def num2(random, total):
   else:
     total=total*0.2
     print("Total de descuento: "+str(total)) 
+  print("######################################################")  
+
+# Ejercicio 3
+def num3(monto):
+  if monto < 50000:
+    monto=monto*0.03
+    print("Total de descuento: "+str(monto))
+  else:
+    monto=monto*0.02
+    print("Total cuota a pagar: "+str(monto)) 
   print("######################################################")  
